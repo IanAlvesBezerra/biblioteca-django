ca092fd36c363dd34ed9a47a91c3273c07657272
Ian Alves Bezerra <IanBezerra.IA@gmail.com>
2024-09-23 10:58:03 -0300

    Atualização do projeto ModelSerializers e ViewSets

--- biblioteca/core/urls.py
+++ biblioteca/core/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework.routers import SimpleRouter
+
+router = SimpleRouter()
+router.register(r"livros", views.LivroViewSet)
+router.register(r"autores", views.AutorViewSet)
+router.register(r"categorias", views.CategoriaViewSet)
 
 urlpatterns = [
-    path('livros/', views.livro_list_create, name='livros-list-create'),
-    path('livros/<int:pk>/', views.livro_detail, name='livro-detail'),
+    path('',include(router.urls)),
+    path('', views.ApiRoot.as_view(), name=views.ApiRoot.name),
 ]
--- biblioteca/core/views.py
+++ biblioteca/core/views.py
@@ -1,43 +1,31 @@
-from django.shortcuts import render
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.decorators import api_view
+from .models import Livro, Autor, Categoria
+from .serializers import LivroSerializer, AutorSerializer, CategoriaSerializer
+from rest_framework import viewsets, generics
 from rest_framework.response import Response
-from rest_framework import status
-from .models import Livro
-from .serializers import LivroSerializer
+from rest_framework.reverse import reverse
 
 # Create your views here.
 
-@api_view(('GET', 'POST',))
-@csrf_exempt
-def livro_list_create(request):
-    if request.method == 'GET':
-        livros = Livro.objects.all()
-        serializer = LivroSerializer(livros, many=True)
-        return Response(serializer.data)
-    
-    if request.method == 'POST':
-        serializer = LivroSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class ApiRoot(generics.GenericAPIView):
+    name = "api-root"
 
-@api_view(('GET', 'PUT', 'DELETE',))
-@csrf_exempt
-def livro_detail(request, pk):
-    livro = Livro.objects.get(pk=pk)
-    if request.method == 'GET':
-        serializer = LivroSerializer(livro)
-        return Response(serializer.data)
- 
-    if request.method == 'PUT':
-        serializer = LivroSerializer(livro, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
- 
-    if request.method == 'DELETE':
-        livro.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def get(self, request, *args, **kwargs):
+        return Response(
+            {
+                "livros": reverse("livro-list", request=request),
+                "autores": reverse("autor-list", request=request),
+                "categorias": reverse("categoria-list", request=request),
+            }
+        )
+
+class LivroViewSet(viewsets.ModelViewSet):
+    queryset = Livro.objects.all()
+    serializer_class = LivroSerializer
+
+class AutorViewSet(viewsets.ModelViewSet):
+    queryset = Autor.objects.all()
+    serializer_class = AutorSerializer
+
+class CategoriaViewSet(viewsets.ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
