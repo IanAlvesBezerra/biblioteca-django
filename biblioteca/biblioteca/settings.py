bca6f9038220ba741c53a0b2a716afc2e722cb70
Ian Alves Bezerra <IanBezerra.IA@gmail.com>
2024-10-06 16:11:29 -0300

    Exercício prático II - CBV, paginação, busca e ordenação

--- biblioteca/biblioteca/settings.py
+++ biblioteca/biblioteca/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # Django REST framework
     'rest_framework',
+    # Django Filters
+    "django_filters",
     # Core application
     'core.apps.CoreConfig',
 ]
@@ -125,3 +127,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "PAGE_SIZE": 5,
+    "DEFAULT_FILTER_BACKENDS": (
+        "django_filters.rest_framework.DjangoFilterBackend",
+        "rest_framework.filters.OrderingFilter",
+        "rest_framework.filters.SearchFilter",
+    ),
+}
--- biblioteca/core/views.py
+++ biblioteca/core/views.py
@@ -3,6 +3,7 @@ from .serializers import LivroSerializer, AutorSerializer, CategoriaSerializer
 from rest_framework import viewsets, generics
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
+from core.filters import LivroFilter
 
 # Create your views here.
 
@@ -18,14 +19,45 @@ class ApiRoot(generics.GenericAPIView):
             }
         )
 
-class LivroViewSet(viewsets.ModelViewSet):
+class LivroList(generics.ListCreateAPIView):
     queryset = Livro.objects.all()
     serializer_class = LivroSerializer
+    name = "livro-list"
+    filterset_class = LivroFilter
+    ordering_fields = (
+        'titulo',
+        'autor',
+        'categoria',
+        'publicado_em',
+    )
 
-class AutorViewSet(viewsets.ModelViewSet):
+class LivroDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Livro.objects.all()
+    serializer_class = LivroSerializer
+    name = "livro-detail"
+
+
+class AutorList(generics.ListCreateAPIView):
     queryset = Autor.objects.all()
     serializer_class = AutorSerializer
+    name = "autor-list"
+    search_fields = ("^nome",)
+    ordering_fields = ('nome',)
 
-class CategoriaViewSet(viewsets.ModelViewSet):
+
+class AutorDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Autor.objects.all()
+    serializer_class = AutorSerializer
+    name = "autor-detail"
+
+class CategoriaList(generics.ListCreateAPIView):
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
+    name = "categoria-list"
+    search_fields = ("^nome",)
+    ordering_fields = ('nome',)
+
+class CategoriaDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+    name = "categoria-detail"
